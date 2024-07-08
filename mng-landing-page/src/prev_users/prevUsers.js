cd1bf72fd10079497e2aff661606b66bec2a8c84
Almaha <mahaw999@gmail.com>
2024-07-08 23:16:36 +0300

    adding prev users code

--- mng-landing-page/src/prev_users/prevUsers.js
+++ mng-landing-page/src/prev_users/prevUsers.js
@@ -1,7 +1,22 @@
+import ReviewCard from "./review_card"
+import GetStartBtn from '../buttons/startBtn'
+import RImg1 from '../images/avatar-ali.png'
+import RImg2 from '../images/avatar-anisha.png'
+import RImg3 from '../images/avatar-shanai.png'
+
 function PrevUsers(){
     return(
-        <div>
-            <h1></h1>
+        <div className='mt-24'>
+            <h1 className='font-bold text-3xl text-center text-indigo-950 mb-10'>What they've said</h1>
+            <section className='flex flex-wrap mx-auto snap-x snap-mandatory overflow-x-hidden'>
+                <ReviewCard className='snap-start flex-shrink-0' imgUrl={RImg1} name={'Ali Bravo'} comment={'We have been able to cancel so many other subscriptions since usnig Manage.'}/>
+                <ReviewCard className='snap-start flex-shrink-0' imgUrl={RImg2} name={'Anisha Li'} comment={'We have been able to cancel so many other subscriptions since usnig Manage.'}/>
+                <ReviewCard className='snap-start flex-shrink-0' imgUrl={RImg3} name={'Shanai Who'} comment={'We have been able to cancel so many other subscriptions since usnig Manage.'}/>
+                <ReviewCard className='snap-start flex-shrink-0' imgUrl={RImg3} name={'Shanai Who'} comment={'We have been able to cancel so many other subscriptions since usnig Manage.'}/>
+            </section>
+            <section className='text-center m-8'>
+               <GetStartBtn /> 
+            </section> 
         </div>
     )
 }
---
+++ mng-landing-page/src/prev_users/review_card.js
@@ -0,0 +1,10 @@
+function ReviewCard({imgUrl, name, comment}){
+    return(
+        <div className='flex flex-col items-center text-center gap-2 m-5 p-5 w-[30%] bg-gray-50'>
+            <img src={imgUrl} alt="User Image" hight='20%' width='20%' className=''/>
+            <p className='text-indigo-950 font-bold text-sm'>{name}</p>
+            <p className='text-gray-400'>{comment}</p>
+        </div>
+    )
+}
+export default ReviewCard
